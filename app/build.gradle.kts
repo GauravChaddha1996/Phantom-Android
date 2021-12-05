739a22c86d24d0588f7b0e9e29a27f5d8b4d048f
Gaurav Chaddha <chaddhag@gmail.com>
2021-12-05 19:07:57 +0530

    Improve dependency gradle stuff

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+import com.project.phantom.buildsrc.Libs
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    defaultConfig {
+        applicationId = "com.project.phantom"
+        compileSdk = 31
+        minSdk = 21
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile(name = "proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Libs.Compose.version
+    }
+}
+
+dependencies {
+    // Basic dependencies
+    implementation(Libs.AndroidX.core)
+    implementation(Libs.AndroidX.appCompat)
+    implementation(Libs.AndroidX.material)
+    implementation(Libs.AndroidX.lifecycleRuntime)
+
+    // Compose related dependencies
+    implementation(Libs.Compose.ui)
+    implementation(Libs.Compose.material)
+    implementation(Libs.Compose.runtime)
+    implementation(Libs.Compose.activityIntegration)
+    implementation(Libs.Compose.runtimeLivedata)
+    implementation(Libs.Compose.uiTooling)
+    debugImplementation(Libs.Compose.uiToolingPreview)
+
+    // Moshi
+    implementation(Libs.Moshi.core)
+    implementation(Libs.Moshi.kotlin)
+    implementation(Libs.Moshi.adapters)
+    kapt(Libs.Moshi.codegen)
+
+    // Koin
+    implementation(Libs.Koin.core)
+    implementation(Libs.Koin.android)
+    implementation(Libs.Koin.compose)
+
+    // Retrofit
+    implementation(Libs.Retrofit.core)
+    implementation(Libs.Retrofit.moshiConverter)
+
+    // Coil
+    implementation(Libs.Coil.core)
+    implementation(Libs.Coil.compose)
+
+    // Swipe refresh
+    implementation(Libs.Accompanist.swipeRefresh)
+
+    // Test related
+    testImplementation(Libs.Test.junit_core)
+    androidTestImplementation(Libs.Test.junitTextExt)
+    androidTestImplementation(Libs.Test.espresso)
+    androidTestImplementation(Libs.Test.composeTest)
+}
---
+++ buildSrc/src/main/java/com/project/phantom/buildsrc/Dependencies.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.project.phantom.buildsrc
+
+object Libs {
+    object AndroidX {
+        // Allows access to new APIs on older API versions of the platform
+        const val core = "androidx.core:core-ktx:1.7.0"
+        const val appCompat = "androidx.appcompat:appcompat:1.4.0"
+
+        const val material = "com.google.android.material:material:1.4.0"
+        const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:2.4.0"
+        const val compose = "androidx.activity:activity-compose:1.4.0"
+    }
+
+    object Accompanist {
+        const val swipeRefresh = "com.google.accompanist:accompanist-swiperefresh:0.18.0"
+    }
+
+    object Compose {
+        const val version = "1.0.5"
+        const val ui = "androidx.compose.ui:ui:$version"
+        const val material = "androidx.compose.material:material:$version"
+        const val activityIntegration = "androidx.activity:activity-compose:1.4.0"
+        const val runtime = "androidx.compose.runtime:runtime:$version"
+        const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$version"
+        const val uiTooling = "androidx.compose.ui:ui-tooling:$version"
+        const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:$version"
+    }
+
+    object Moshi {
+        private const val version = "1.12.0"
+        const val core = "com.squareup.moshi:moshi:$version"
+        const val kotlin = "com.squareup.moshi:moshi-kotlin:$version"
+        const val codegen = "com.squareup.moshi:moshi-kotlin-codegen:$version"
+        const val adapters = "com.squareup.moshi:moshi-adapters:$version"
+    }
+
+    object Koin {
+        private const val version = "3.1.2"
+        const val core = "io.insert-koin:koin-core:$version"
+        const val android = "io.insert-koin:koin-android:$version"
+        const val compose = "io.insert-koin:koin-androidx-compose:$version"
+    }
+
+    object Retrofit {
+        private const val version = "2.9.0"
+        const val core = "com.squareup.retrofit2:retrofit:$version"
+        const val moshiConverter = "com.squareup.retrofit2:converter-moshi:$version"
+    }
+
+    object Coil {
+        private const val version = "1.4.0"
+        const val core = "io.coil-kt:coil:$version"
+        const val compose = "io.coil-kt:coil-compose:$version"
+    }
+
+    object Test {
+        const val junit_core = "junit:junit:4.13.2"
+        const val junitTextExt = "androidx.test.ext:junit:1.1.3"
+        const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
+        const val composeTest = "androidx.compose.ui:ui-test-junit4:${Compose.version}"
+
+    }
+}
